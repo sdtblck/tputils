409dc7baf8837a8db167ec77c215db7d87e3f5ad
sdtblck <46172032+sdtblck@users.noreply.github.com>
2021-04-02 22:44:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r" as f:
+    long_description = f.read()
+          
 setup(
     name='tputils',
     packages=find_packages(),
     version='0.0.1',
     license='MIT',
     description='Utilities for TPUs.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Sid Black',
     author_email='sdtblck@gmail.com',
     install_requires=[
